1fb7d0aa1c80b28eef2836fc38efb4e8e07ce77f
rymikula <rymikula@gmail.com>
2025-03-24 20:24:37 -0400

    Removed temp install scripts

--- new-frontend/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Run this script to install dependencies with the legacy peer dependencies flag
-npm install --legacy-peer-deps
-
-echo "Dependencies installed successfully with legacy peer dependencies flag."
-echo "Run 'npm run dev' to start the development server." 
