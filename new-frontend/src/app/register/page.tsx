f72c967436ebf54fb5cb62206fd67c4614220f48
O'Neal M'Beri <clarkoneal@gmail.com>
2025-04-14 18:59:03 -0400

    changing of a different color registeration page

--- new-frontend/src/app/register/page.tsx
+++ new-frontend/src/app/register/page.tsx
@@ -2,8 +2,9 @@ import { RegisterForm } from "@/components/register-form";
 
 export default function RegisterPage() {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-gray-100 to-gray-200 dark:from-gray-900 dark:to-gray-800">
+    <main className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-indigo-200 to-sky-200 dark:from-indigo-900 dark:to-sky-900">
       <RegisterForm />
     </main>
   );
-} 
+}
+
